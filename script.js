b7dc4c408881eef009570c9cf153dfe6e7fdf413
Emiliaa1 <75319625+Emiliaa1@users.noreply.github.com>
2024-08-02 17:48:32 +0300

    Menu dark overlay + search bar

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 const offScreenMenu = document.querySelector('.off-screen-menu');
 const menu = document.querySelector('#menu');
 const cross = document.querySelector('#cross')
+const overlay = document.querySelector('.dark-filter');
 
 menu.addEventListener('click', ()=> {
-    offScreenMenu.classList.toggle('active');
+    offScreenMenu.classList.add('active');
+    overlay.classList.add('active');
 })
 
 cross.addEventListener('click',()=>{
-    offScreenMenu.classList.toggle('active');
+    offScreenMenu.classList.remove('active');
+    overlay.classList.remove('active');
+})
+
+const search = document.querySelector('#search');
+const searchBar = document.querySelector('.search-bar');
+const crossSearch = document.querySelector('#cross-search');
+
+search.addEventListener('click',()=>{
+    searchBar.classList.add('active');
+})
+
+crossSearch.addEventListener('click',()=>{
+    searchBar.classList.remove('active');
 })
