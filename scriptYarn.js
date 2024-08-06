5365ad4e591b857693157e45081b4b00c744039a
Emiliaa1 <75319625+Emiliaa1@users.noreply.github.com>
2024-08-06 12:37:46 +0300

    no folders + yarn page + json products try successful

--- scriptYarn.js
+++ scriptYarn.js
@@ -24,3 +24,34 @@ search.addEventListener('click',()=>{
 crossSearch.addEventListener('click',()=>{
     searchBar.classList.remove('active');
 })
+
+
+//Create an  xmlhttp-request object
+
+let http = new XMLHttpRequest();
+http.open('get','yarns.json',true);
+http.send();
+
+http.onload = function(){
+    if(this.readyState == 4 && this.status == 200){
+
+        let products = JSON.parse(this.responseText);
+        let output = "";
+
+        for(let item of products){
+            output+=`
+                <div class = "product">
+                    <img src="${item.image}" alt="${item.image}">
+                    <p class="name">${item.name}</p>
+                    <p class="price">
+                        <span>${item.price}</span>
+                        <span>&euro;</span>
+                    </p>
+                </div>
+            `;
+        }
+
+        document.querySelector(".products").innerHTML = output;
+
+    }
+}
